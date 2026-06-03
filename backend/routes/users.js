a19a248d28f885e9eedd429611ccb3dc7d3bb8bd
gina eifman <jina-3@ya.ru>
2026-06-03 07:22:33 +0300

    fix: gridfs db connection

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,12 +1,12 @@
 const router = require('express').Router();
 const { getProfileInfo, getAvatar, editProfileInfo } = require('../controllers/users');
-const upload = require('../middlewares/upload');
+const { initUpload } = require('../middlewares/upload');
 const { validateEditProfileInfo } = require('../middlewares/validation');
 
 router.get('/me', getProfileInfo);
 router.get('/avatar/:id', getAvatar);
 router.patch('/me', (req, res, next) => {
-  const upload = upload.initUpload();
+  const upload = initUpload();
   return upload.single('avatar')(req, res, next);
 }, validateEditProfileInfo, editProfileInfo);
 
